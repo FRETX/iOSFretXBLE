0d8dba98845842d4ac5e8c8632419ac169f14894
Onur Babacan <onur@onurs-imac.local>
2017-06-19 23:46:07 +0300

    Initial Commit

---
+++ FretxBLE/FretxBLE.h
@@ -0,0 +1,19 @@
+//
+//  FretxBLE.h
+//  FretxBLE
+//
+//  Created by Onur Babacan on 19/06/2017.
+//  Copyright © 2017 Onur Babacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FretxBLE.
+FOUNDATION_EXPORT double FretxBLEVersionNumber;
+
+//! Project version string for FretxBLE.
+FOUNDATION_EXPORT const unsigned char FretxBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FretxBLE/PublicHeader.h>
+
+
