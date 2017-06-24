d446ce6a82b81e6db2b802f7f7c16db12b68346f
Onur Babacan <onur@Onurs-iMac.local>
2017-06-24 12:03:31 +0300

    added clear()

--- FretxBLE/FretxBLE.swift
+++ FretxBLE/FretxBLE.swift
@@ -41,6 +41,10 @@ public class FretxBLE: NSObject {
         bleManager.connectToFretX()
     }
     
+    public func clear(){
+        bleManager.sendToFretX(fretCodes: [0])
+    }
+    
     public func connectKeepTrying(){
         
     }
